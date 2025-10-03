13533386d6214d4447e8777fe52cdad27bdb3fa0
Staryannfree <74217365+Staryannfree@users.noreply.github.com>
2025-10-03 13:18:47 -0300

    Update defaults.php

--- inc/shortcodes/defaults.php
+++ inc/shortcodes/defaults.php
@@ -1,34 +1,31 @@
 <?php
 /**
-* Defaults/Fallbacks de atributos para shortcodes.
-* Permite usar ?restaurant_id=123 na URL quando o atributo não for passado.
-*
-* @package VemComerCore
-*/
-
+ * Defaults/Fallbacks de atributos para shortcodes.
+ * Permite usar ?restaurant_id=123 na URL quando o atributo não for passado.
+ *
+ * @package VemComerCore
+ */
 
 if ( ! defined( 'ABSPATH' ) ) { exit; }
 
-
 // [vc_restaurant id="..."] => se "id" não vier, usa ?restaurant_id=
 add_filter( 'shortcode_atts_vc_restaurant', function ( $out, $pairs, $atts ) {
-if ( empty( $out['id'] ) ) {
-$qs = isset( $_GET['restaurant_id'] ) ? absint( wp_unslash( $_GET['restaurant_id'] ) ) : 0;
-if ( $qs ) {
-$out['id'] = $qs;
-}
-}
-return $out;
+    if ( empty( $out['id'] ) ) {
+        $qs = isset( $_GET['restaurant_id'] ) ? absint( wp_unslash( $_GET['restaurant_id'] ) ) : 0;
+        if ( $qs ) {
+            $out['id'] = $qs;
+        }
+    }
+    return $out;
 }, 10, 3 );
-
 
 // [vc_menu_items restaurant="..."] => se "restaurant" não vier, usa ?restaurant_id=
 add_filter( 'shortcode_atts_vc_menu_items', function ( $out, $pairs, $atts ) {
-if ( empty( $out['restaurant'] ) ) {
-$qs = isset( $_GET['restaurant_id'] ) ? absint( wp_unslash( $_GET['restaurant_id'] ) ) : 0;
-if ( $qs ) {
-$out['restaurant'] = $qs;
-}
-}
-return $out;
+    if ( empty( $out['restaurant'] ) ) {
+        $qs = isset( $_GET['restaurant_id'] ) ? absint( wp_unslash( $_GET['restaurant_id'] ) ) : 0;
+        if ( $qs ) {
+            $out['restaurant'] = $qs;
+        }
+    }
+    return $out;
 }, 10, 3 );
