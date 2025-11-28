e0019c1457461194dc2e474567a688165fb7231a
Staryannfree <74217365+Staryannfree@users.noreply.github.com>
2025-11-28 01:46:23 -0300

    Ensure restaurant cards use canonical permalinks

---
+++ templates/marketplace/gestor-eventos.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template Name: Marketplace - Gestor Eventos
+ * Description: Placeholder for a dynamic version of templates/marketplace/gestor-eventos.html.
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+get_header();
+?>
+<div class="vc-marketplace-placeholder">
+    <p><?php esc_html_e('Placeholder for the Gestor de Eventos template.', 'vemcomer'); ?></p>
+</div>
+<?php
+get_footer();
---
+++ templates/marketplace/todas-as-categorias.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template Name: Marketplace - Todas as Categorias
+ * Description: Placeholder for a dynamic version of templates/marketplace/todas-as-categorias.html.
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+get_header();
+?>
+<div class="vc-marketplace-placeholder">
+    <p><?php esc_html_e('Placeholder for the Todas as Categorias template.', 'vemcomer'); ?></p>
+</div>
+<?php
+get_footer();
