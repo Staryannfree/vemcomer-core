5e5e3f3c201d1b042d61e3756e4e286a5883d772
Staryannfree <74217365+Staryannfree@users.noreply.github.com>
2025-11-28 03:59:53 -0300

    Render marketplace template pages from static mocks

---
+++ templates/marketplace/detalhes-evento.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Template Name: Marketplace - Detalhes Evento
+ * Description: Placeholder for a dynamic version of templates/marketplace/detalhes-evento.html.
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+$vc_marketplace_inline = defined('VC_MARKETPLACE_INLINE') && VC_MARKETPLACE_INLINE;
+
+require_once __DIR__ . '/static-loader.php';
+
+if (! $vc_marketplace_inline) {
+    get_header();
+}
+
+vc_marketplace_render_static_template('detalhes-evento.html');
+
+if (! $vc_marketplace_inline) {
+    get_footer();
+}
